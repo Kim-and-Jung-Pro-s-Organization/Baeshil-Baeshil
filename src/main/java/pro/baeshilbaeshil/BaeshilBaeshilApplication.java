5dd5fb98c42ba64014f6d2a996551c25bd490424
eunaJung01 <christinejung10@daum.net>
2025-01-11 17:55:15 +0900

    initial commit

---
+++ src/main/java/pro/baeshilbaeshil/BaeshilBaeshilApplication.java
@@ -0,0 +1,13 @@
+package pro.baeshilbaeshil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeshilBaeshilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeshilBaeshilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/baeshilbaeshil/BaeshilBaeshilApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.baeshilbaeshil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeshilBaeshilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
