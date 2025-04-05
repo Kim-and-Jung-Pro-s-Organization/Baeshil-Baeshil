0c6d0115508afecf67e58ee35a1f215bfec71b93
eunaJung01 <christinejung10@daum.net>
2025-04-05 21:44:15 +0900

    build: Spring REST Docs 설정

--- src/test/java/pro/baeshilbaeshil/common/AcceptanceTest.java
+++ src/test/java/pro/baeshilbaeshil/common/AcceptanceTest.java
@@ -2,13 +2,21 @@ package pro.baeshilbaeshil.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.restassured.RestAssuredRestDocumentation;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ActiveProfiles("test")
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTest {
 
@@ -21,9 +29,17 @@ public abstract class AcceptanceTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
+    protected RequestSpecification documentationSpec;
+
     @BeforeEach
-    public void setUp() {
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
         RestAssured.port = port;
         databaseCleanup.execute();
+
+        documentationSpec = new RequestSpecBuilder()
+                .setBaseUri("http://localhost")
+                .setPort(port)
+                .addFilter(RestAssuredRestDocumentation.documentationConfiguration(restDocumentation))
+                .build();
     }
 }
